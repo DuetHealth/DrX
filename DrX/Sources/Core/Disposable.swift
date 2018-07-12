ba503ddf6baa2934c5029ae117543ed8b1de86b2
Ryan Wachowski <ryan.wachowski@duethealth.com>
2018-07-12 14:11:04 -0400

    Adds method for tying disposables to the lifetimes of objects without needing to pollute every type with a property

---
+++ DrX/Sources/Core/Disposable.swift
@@ -0,0 +1,21 @@
+import Foundation
+import RxSwift
+
+fileprivate var disposableKey = UInt8.max
+
+public extension Disposable {
+
+    /// Retains the resources associated with a subscription until the argument object is deallocated.
+    ///
+    /// - Parameter object: the object to which this disposable's lifetime is tethered.
+    public func withLifetime(of object: AnyObject) {
+        if let bag = objc_getAssociatedObject(object, &disposableKey) as? DisposeBag {
+            bag.insert(self)
+            return
+        }
+        let bag = DisposeBag()
+        bag.insert(self)
+        objc_setAssociatedObject(object, &disposableKey, bag, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+
+}
