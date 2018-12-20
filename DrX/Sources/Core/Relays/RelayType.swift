db2143d5f319a8a5a7610dbd3a412321c5f38947
ライアン <ryan.wachowski@duethealth.com>
2018-12-20 12:49:31 -0500

    Adds protocol definition for relays and a type-erasing relay construct

---
+++ DrX/Sources/Core/Relays/RelayType.swift
@@ -0,0 +1,18 @@
+//
+//  RelayType.swift
+//  DrX
+//
+//  Created by Ryan Wachowski on 8/22/18.
+//  Copyright © 2018 Duet Health. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+/// Represents a push style sequence which may also observe other sequences or raw values by directly
+/// accepting them into the sequence.
+public protocol RelayType: ObservableType {
+    func asObserver() -> AnyObserver<E>
+    func accept(_ event: E)
+}
