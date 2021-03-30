b3a37a021ebd945daa4f8c0803fd8f8f5eb31e20
KZ <kesi.maduka@duethealth.com>
2021-03-30 09:34:33 -0400

    Bump dependency versions.

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "DrX",
     platforms: [.iOS(.v8)],
     products: [.library(name: "DrX", targets: ["DrX"])],
-    dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
-                   .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-                   .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
+    dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+                   .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
+                   .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
                   ],
     targets: [
         .target(name: "DrX", dependencies: ["RxSwift", "RxCocoa", "RxRelay"], path: "DrX/Sources", exclude: ["DrX/Sources/DrX.h"]),
