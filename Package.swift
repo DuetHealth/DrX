5dbcaa3d5b36c74c369ac544f3cf354f0b58c557
KZ <kesi.maduka@duethealth.com>
2021-03-30 10:04:52 -0400

    Bump minimum iOS version.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "DrX",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v9)],
     products: [.library(name: "DrX", targets: ["DrX"])],
     dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
                    .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0"),
