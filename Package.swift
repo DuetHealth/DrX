37a9f76c433523bd4638bfcb5dcbf303e86e0811
KZ <github@kez.io>
2021-03-30 10:04:13 -0400

    Revert "Increase minimum supported iOS version."
    This reverts commit 9bd4e6585ee43e6547d2709f802f51229f470797.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "DrX",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v8)],
     products: [.library(name: "DrX", targets: ["DrX"])],
     dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
                    .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
