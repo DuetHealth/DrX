d6b6716bdbfa98803d42809d7d34ad1e811b4557
Pavan <kumar.pavan@rakuten.com>
2022-03-05 14:03:27 +0900

    - Set RxSwift to a specific version

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ let package = Package(
     name: "DrX",
     platforms: [.iOS(.v12)],
     products: [.library(name: "DrX", targets: ["DrX"])],
-    dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0"),
+    dependencies: [.package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
                    .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
                    .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
                   ],
     targets: [
-        .target(name: "DrX", dependencies: ["RxSwift", "RxCocoa", "RxRelay"], path: "DrX/Sources", exclude: ["DrX/Sources/DrX.h"]),
+        .target(name: "DrX", dependencies: ["RxSwift", "RxCocoa", "RxRelay"], path: "DrX/Sources", exclude: ["DrX.h"]),
         .testTarget(name: "DrXTests", dependencies: ["DrX", "Nimble", "Quick"], path: "DrXTests/Sources"),
     ]
 )
